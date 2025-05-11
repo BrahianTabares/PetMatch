f9df006dadebde2e653f6e720180296da9089333
daniela guzman <danielaguzman50@outlook.es>
2025-05-10 19:06:16 -0500

    ruta

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/PetMatch/", // muy importante para GitHub Pages
+  base: "https://brahiantabares.github.io/PetMatch", // muy importante para GitHub Pages
   plugins: [react()],
 });
